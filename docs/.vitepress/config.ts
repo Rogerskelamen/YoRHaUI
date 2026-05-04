dedcb2d7925e8248a2cc122f4e730cee1f36e043
Rogerskelamen <rogerskelamen@gmail.com>
2026-05-04 21:16:40 +0800

    docs(dev): change dev logs to vitepress space

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,28 +1,39 @@
-import { defineConfig } from 'vitepress'
+// TODO: find out how to use vitepress to maintain docs website
+import { defineConfig } from 'vitepress';
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  title: "YoRHa UI",
-  description: "A Vue 3 component library inspired by YoRHa UI (NieR: Automata)",
+  title: 'YoRHa UI',
+  description:
+    'A Vue 3 component library inspired by YoRHa UI (NieR: Automata)',
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
       { text: 'Home', link: '/' },
-      { text: 'Examples', link: '/markdown-examples' }
+      { text: 'Examples', link: '/markdown-examples' },
+      { text: '开发', link: '/development-logs' },
     ],
 
-    sidebar: [
-      {
-        text: 'Examples',
-        items: [
-          { text: 'Markdown Examples', link: '/markdown-examples' },
-          { text: 'Runtime API Examples', link: '/api-examples' }
-        ]
-      }
-    ],
+    sidebar: {
+      '/': [
+        {
+          text: 'Examples',
+          items: [
+            { text: 'Markdown Examples', link: '/markdown-examples' },
+            { text: 'Runtime API Examples', link: '/api-examples' },
+          ],
+        },
+      ],
+      '/dev/': [
+        {
+          text: '开发',
+          items: [{ text: '开发日志', link: '/development-logs' }],
+        },
+      ],
+    },
 
     socialLinks: [
-      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
-    ]
-  }
-})
+      { icon: 'github', link: 'https://github.com/vuejs/vitepress' },
+    ],
+  },
+});
