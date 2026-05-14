922f8bb3433846684ba0e6ba3c3fdaf7f52469ee
Rogerskelamen <rogerskelamen@gmail.com>
2026-05-14 10:24:52 +0800

    style(font): pack 'Source Sans 3' font at local

--- playground/App.vue
+++ playground/App.vue
@@ -1,6 +1,15 @@
 <template>
   <h1>YoRHa UI Playground</h1>
-  <div>
-    <yrh-button />
-  </div>
+
+  <br />
+
+  <yrh-button>Click Me</yrh-button>
+
+  <br />
+
+  <p>
+    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quam, illum
+    voluptate ducimus minima libero optio soluta non corrupti laudantium saepe
+    error tenetur, a delectus. In perferendis expedita ullam explicabo id!
+  </p>
 </template>
