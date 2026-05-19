0c3200e6c75c703a7008e1224384382ba7002f81
Rogerskelamen <rogerskelamen@gmail.com>
2026-05-19 19:44:21 +0800

    build(bundle): use vite to build package

---
+++ vite.lib.config.ts
@@ -0,0 +1,56 @@
+import { resolve } from 'path';
+import vue from '@vitejs/plugin-vue';
+import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
+
+export default defineConfig({
+  plugins: [
+    vue(),
+    dts({
+      insertTypesEntry: true,
+      include: ['packages'],
+      exclude: ['playground'],
+    }),
+  ],
+
+  resolve: {
+    alias: {
+      'yorha-ui': resolve(__dirname, 'packages/yorha-ui'),
+      '@yorha-ui/components': resolve(__dirname, 'packages/components'),
+      '@yorha-ui/utils': resolve(__dirname, 'packages/utils'),
+    },
+  },
+
+  build: {
+    outDir: 'dist',
+
+    lib: {
+      entry: resolve(__dirname, 'packages/yorha-ui/index.ts'),
+      name: 'YoRHaUI',
+      fileName: (format) => {
+        switch (format) {
+          case 'es':
+            return 'index.esm.js';
+
+          case 'cjs':
+            return 'index.cjs.js';
+
+          default:
+            return `index.${format}.js`;
+        }
+      },
+      formats: ['es', 'cjs'],
+    },
+
+    rollupOptions: {
+      external: ['vue'],
+
+      output: {
+        exports: 'named',
+        globals: {
+          vue: 'Vue',
+        },
+      },
+    },
+  },
+});
